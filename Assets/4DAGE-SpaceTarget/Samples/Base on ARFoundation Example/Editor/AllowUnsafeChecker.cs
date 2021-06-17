3edcb8cb047f124692c41e6ba4eec33941c24424
zwt <289096885@qq.com>
2021-06-17 19:05:43 +0800

    Add ARFoundation auto package config

---
+++ Assets/4DAGE-SpaceTarget/Samples/Base on ARFoundation Example/Editor/AllowUnsafeChecker.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+public class AllowUnsafeChecker 
+{
+    [InitializeOnLoadMethod]
+    private static void AllowUCSetting()
+    {
+        if (!PlayerSettings.allowUnsafeCode)
+            PlayerSettings.allowUnsafeCode = true;
+    }
+}
