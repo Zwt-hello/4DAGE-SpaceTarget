f1f246ac89a5f9844d45281ed718fde54f63e674
zwt <289096885@qq.com>
2021-06-30 12:25:57 +0800

    API and datas update

--- Runtime/ARBaseInterface/IARBaseProvider.cs
+++ Runtime/ARBaseInterface/IARBaseProvider.cs
@@ -1,8 +1,15 @@
 ﻿
 namespace SpaceTarget.Runtime
 {
+    /// <summary>
+    /// ARBase provider
+    /// </summary>
     public interface IARBaseProvider
     {
+        /// <summary>
+        /// Create 
+        /// </summary>
+        /// <returns>The implemention of ARBase</returns>
         IARBase Create();
     }
 }
---
+++ Runtime/ARBaseInterface/Types/ARBaseCameraIntrinsics.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace SpaceTarget.Runtime
+{
+    /// <summary>
+    /// AR camera intrinsics
+    /// </summary>
+    public struct ARBaseCameraIntrinsics
+    {
+        /// <summary>
+        /// Raw image resolution
+        /// </summary>
+        public Vector2Int resolution;
+
+        /// <summary>
+        /// Camera's focal length
+        /// </summary>
+        public Vector2 focalLength;
+
+        /// <summary>
+        /// Raw image principal point
+        /// </summary>
+        public Vector2 principalPoint;
+    }
+}
